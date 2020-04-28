7ecc885787a851b4adbee76d832f1697aa331c90
lang-sky <skylang121@outlook.com>
2020-04-28 22:41:46 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React from "react";
+import {Flex} from './Flex'
+import "./styles.css";
+
+const CardContainerStyle = {
+  width: 300,
+  margin: '32px auto',
+  borderRadius: 5,
+  display: 'flex',
+}
+
+const ImageStyle = {
+  width: 65,
+  height: 65,
+  borderRadius: 65
+}
+
+const RightColumn = {
+  flex: 1,
+  marginLeft: 16
+}
+
+const NameAndDate = {
+  display: 'flex',
+  justifyContent: 'space-between',
+}
+
+const Message = {
+  fontSize: 14,
+}
+
+const NameStyle = {
+  margin: 0
+}
+
+const DateStyle = {
+  margin: 0,
+  color: 'grey',
+  fontWeight: 'normal'
+}
+
+export default function App() {
+  return (
+    <>
+    <MessageNoFlex/>
+    <MessageWithFlex/>
+    </>
+  );
+}
+
+const MessageNoFlex = () => (
+<div style={CardContainerStyle}>
+      <img 
+        style={ImageStyle} 
+        src="https://discountdoorhardware.ca/wp-content/uploads/2018/06/profile-placeholder-3.jpg"
+        alt="Profile"
+      />
+      <div style={RightColumn}>
+        <div style={NameAndDate}>
+          <h4 style={NameStyle}> John Doe </h4>
+          <h5 style={DateStyle}> 5 Hours Ago </h5>
+        </div>
+        <p style={Message}>
+        This message DOES the Flex component.
+        </p>
+      </div>
+    </div>
+)
+
+const MessageWithFlex = () => (
+  <Flex container width="300px" margin="32px auto">
+    <img 
+        style={ImageStyle} 
+        src="https://discountdoorhardware.ca/wp-content/uploads/2018/06/profile-placeholder-3.jpg"
+        alt="Profile"
+    />
+    <Flex flex={1} margin="0 0 0 16px"> 
+      <Flex container justifyContent="space-between">
+          <h4 style={NameStyle}> John Doe </h4>
+          <h5 style={DateStyle}> 5 Hours Ago </h5>
+      </Flex>
+      <p style={Message}>
+        This message does not use the Flex component.
+      </p>
+    </Flex>
+  </Flex>
+)
